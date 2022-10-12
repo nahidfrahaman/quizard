16acfea27b8037b05bc22bb0a830e21903997b36
Nahidfrahaman <nahi.frahaman@gmail.com>
2022-10-12 19:12:26 +0600

    Header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,24 +24,22 @@ import animation from "../assets/animation.json";
         <div className="relative flex flex-col items-start justify-center w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
           <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
             <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-              Brand new
+             
             </p>
             <h2 className="mb-5 font-sans text-5xl font-bold tracking-tight text-[#007D7C] sm:text-4xl sm:leading-none">
-              Everything you
+                Test Your Self And
               <br className="hidden md:block" />
-              can imagine{' '}
+             {' '}
               <span className="inline-block text-deep-purple-accent-400">
-                is real
+                Grow Up
               </span>
             </h2>
             <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae. explicabo.
+            A good test tells a story. It can serve not only as verification that software is behaving predictably, but also as documentation for other developers.
             </p>
             <div className="flex items-center">
               <a
-                href="/"
+                href="/quiz/1"
                 className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-[#007D7C] transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
               >
                 Now Test Your Self
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizSection from './QuizSection';
-
 const Quiz = () => {
+
+
+    
+   
     const quizdata= useLoaderData();
     const quizDatas = quizdata.data
-   console.log(quizDatas)
+   
     return (
         <div>
+            
            <QuizSection quizDatas={quizDatas}></QuizSection>
         </div>
     );
